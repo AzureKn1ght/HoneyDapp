9e331916a9760fb44dfff01c2c3d0d0ee25c7989
Anwar <anwartan8@gmail.com>
2021-09-03 17:32:07 +0800

    updated scripts and contract js file

--- frontend/contract.js
+++ frontend/contract.js
@@ -1,14 +1,13 @@
 
 //Dependencies
-const Web3 = require('web3');
-const GoalsContract = require('./HoneyDapp/frontend/GoalsContract.json');  
-const provider = new HDWalletProvider(
-    privateKey,
-    'https://ropsten.infura.io/v3/5880660930f14d27970e5ae5f5b73e26'
-)
+//const Web3 = require('web3');
+//const GoalsContract = require('./HoneyDapp/frontend/GoalsContract.json');  
+//const web3 = new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/5880660930f14d27970e5ae5f5b73e26');
+var web3 = new Web3('https://ropsten.infura.io/v3/5880660930f14d27970e5ae5f5b73e26'); 
+
 
 //Create Contract instance
-const init = async () => {                               // if using node js need to wrap in async
+const init = async (e) => {                               // if using node js need to wrap in async
 	
     e.preventDefault(); //to prevent form from submitting and refreshing the page
 
@@ -23,21 +22,22 @@ const init = async () => {                               // if using node js nee
     var a = amt.value;
     console.log(`desc: ${desc}, amt: ${a}, id: ${userID}`);
 
-    const web3 = new Web3(provider);
-	
+    //const web3 = new Web3(provider);
+	const conAddress = '0xAe790B847C02280Cb182c55E3ec95C418D1429E8'
 
 	const contract = new web3.eth.Contract(
-	GoalsContract.abi,
+	  abi,
+    conAddress
   	);
 
     //Step 2: Send transaction to smart contract
-
-    await contract.methods.addGoal(addressGoalOwner,a,d).send({
-        from: addressGoalOwner
-    });
+    const addressGoalOwner = '0x4b7d0309042Be687F432B6fB73BCbcd405CD25e5';
+    //await contract.methods.addGoal(addressGoalOwner,a,d).send({
+     //   from: addressGoalOwner
+   // });
 
     //calls the getAllGoals funtion and prints in the console
-    const data = await contract.methods.getAllGoals().call();
+    const data = await contract.methods.getAllGoals(addressGoalOwner).call();
     console.log(data);
 }
 
